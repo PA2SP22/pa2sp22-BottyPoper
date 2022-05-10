6b6aa93defe8e779b953bf8b63c6022e63c33a68
Charles Clarke <cclarke002@student.butte.edu>
2022-05-09 21:16:21 -0700

    3c documentation

--- assignments/3c/todo_ui.h
+++ assignments/3c/todo_ui.h
@@ -1,6 +1,13 @@
+/*
+ * Name        : todo_item.h
+ * Author      : Charles Clarke
+ * Description : functions for todo_item class
+ */
+
 #include "todo_list.h"
 #include "CinReader.h"
 #include "todo_item.h"
+// Todo UI is a minimalist terminal ui tool.
 class TodoUI
 {
 private:
