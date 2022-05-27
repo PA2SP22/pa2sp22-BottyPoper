5b214343ac125d37516a7783bb729668d8989a7c
Charles Clarke <cclarke002@student.butte.edu>
2022-05-26 22:09:29 -0700

    lab 21done

--- labs/21/template.cpp
+++ labs/21/template.cpp
@@ -7,6 +7,8 @@
 #include <iostream>
 #include <sstream>
 #include <string>
+#include <iomanip>
+#include <limits>
 using std::string;
 using std::stringstream;
 
@@ -16,7 +18,8 @@ using std::stringstream;
  * by a specified character. For example, if the array contents
  * are {1, 2, 3} and the separator character is ':', the string returned
  * will be "1:2:3". Please note the array can be of any type.
- * If the template function outputs doubles, they should be to a precision of 2.
+ *
+ * .
  * @uses stringstream
  * @param T values[] - An array of any type
  * @param unsigned int size - The size of the array
@@ -26,28 +29,42 @@ using std::stringstream;
  *                  specified separator character
  */
 template <typename T>
-string PrepareForDisplay(T array[], unsigned int size, char separator) {
-  if (size > 0) {
+string PrepareForDisplay(T array[], unsigned int size, char separator)
+{
+  if (size > 0)
+  {
     stringstream output;
+    output.precision(2);
+    output.setf(std::ios_base::fixed, std::ios_base::floatfield);
     output << array[0];
-    for (unsigned int i = 1; i < size; i++) {
-      output << separator << array[i];
+    for (unsigned int i = 1; i < size; i++)
+    {
+      output  << separator << array[i];
     }
     return output.str();
-  } else {
+  }
+  else
+  {
     return "";
   }
 }
 template <typename T>
-string PrepareForDisplay(T array[], unsigned int size) {
-  if (size > 0) {
+string PrepareForDisplay(T array[], unsigned int size)
+{
+  if (size > 0)
+  {
     stringstream output;
-    output << array[0];
-    for (unsigned int i = 1; i < size; i++) {
-      output << "," << array[i];
+    output.precision(2);
+    output.setf(std::ios_base::fixed, std::ios_base::floatfield);
+    output <<  array[0];
+    for (unsigned int i = 1; i < size; i++)
+    {
+      output <<  "," << array[i];
     }
     return output.str();
-  } else {
+  }
+  else
+  {
     return "";
   }
 }
@@ -60,10 +77,14 @@ string PrepareForDisplay(T array[], unsigned int size) {
  * @return bool - true if value is found in the array, otherwise false
  */
 template <typename T>
-bool HasValue(T array[], unsigned int size, T tofind) {
-  if (size > 0) {
-    for (unsigned int i = 0; i < size; i++) {
-      if (array[i] == tofind) {
+bool HasValue(T array[], unsigned int size, T tofind)
+{
+  if (size > 0)
+  {
+    for (unsigned int i = 0; i < size; i++)
+    {
+      if (array[i] == tofind)
+      {
         return true;
       }
     }
@@ -85,11 +106,15 @@ bool HasValue(T array[], unsigned int size, T tofind) {
  *               you can return T()
  */
 template <typename T>
-T ValueAt(T array[], unsigned int size, unsigned int index, bool &error) {
+T ValueAt(T array[], unsigned int size, unsigned int index, bool &error)
+{
   error = false;
-  if (index < size) {
+  if (index < size)
+  {
     return array[index];
-  } else {
+  }
+  else
+  {
     error = true;
     return T();
   }
@@ -103,9 +128,11 @@ T ValueAt(T array[], unsigned int size, unsigned int index, bool &error) {
  * @return T - The sum of the values in the array
  */
 template <typename T>
-T Sum(T array[], unsigned int size) {
+T Sum(T array[], unsigned int size)
+{
   T output = T();
-  for (unsigned int i; i < size; i++) {
+  for (unsigned int i = 0; i < size; i++)
+  {
     output += array[i];
   }
   return output;
@@ -122,9 +149,17 @@ T Sum(T array[], unsigned int size) {
  */
 template <typename T>
 bool SwapValues(T array[], unsigned int size, unsigned int index1,
-                unsigned int index2) {
-  if (index1 <= size && index2 <= size) {
-  } else {
+                unsigned int index2)
+{
+  if (index1 <= size && index2 <= size)
+  {
+    T temp = array[index1];
+    array[index1] = array[index2];
+    array[index2] = temp;
+    return true;
+  }
+  else
+  {
     return false;
   }
 }
