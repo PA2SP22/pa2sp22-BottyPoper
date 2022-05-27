a19c1c9d72cf7a1c50364d695444fabee9b6fc12
Charles Clarke <cclarke002@student.butte.edu>
2022-05-26 22:12:32 -0700

    lab 21 all done

--- labs/21/template.cpp
+++ labs/21/template.cpp
@@ -4,11 +4,11 @@
  * Description : Various functions using templates. Please note you can just
  *               write the definitions in here. No need for prototypes.
  */
+#include <iomanip>
 #include <iostream>
+#include <limits>
 #include <sstream>
 #include <string>
-#include <iomanip>
-#include <limits>
 using std::string;
 using std::stringstream;
 
@@ -29,42 +29,32 @@ using std::stringstream;
  *                  specified separator character
  */
 template <typename T>
-string PrepareForDisplay(T array[], unsigned int size, char separator)
-{
-  if (size > 0)
-  {
+string PrepareForDisplay(T array[], unsigned int size, char separator) {
+  if (size > 0) {
     stringstream output;
     output.precision(2);
     output.setf(std::ios_base::fixed, std::ios_base::floatfield);
     output << array[0];
-    for (unsigned int i = 1; i < size; i++)
-    {
-      output  << separator << array[i];
+    for (unsigned int i = 1; i < size; i++) {
+      output << separator << array[i];
     }
     return output.str();
-  }
-  else
-  {
+  } else {
     return "";
   }
 }
 template <typename T>
-string PrepareForDisplay(T array[], unsigned int size)
-{
-  if (size > 0)
-  {
+string PrepareForDisplay(T array[], unsigned int size) {
+  if (size > 0) {
     stringstream output;
     output.precision(2);
     output.setf(std::ios_base::fixed, std::ios_base::floatfield);
-    output <<  array[0];
-    for (unsigned int i = 1; i < size; i++)
-    {
-      output <<  "," << array[i];
+    output << array[0];
+    for (unsigned int i = 1; i < size; i++) {
+      output << "," << array[i];
     }
     return output.str();
-  }
-  else
-  {
+  } else {
     return "";
   }
 }
@@ -77,14 +67,10 @@ string PrepareForDisplay(T array[], unsigned int size)
  * @return bool - true if value is found in the array, otherwise false
  */
 template <typename T>
-bool HasValue(T array[], unsigned int size, T tofind)
-{
-  if (size > 0)
-  {
-    for (unsigned int i = 0; i < size; i++)
-    {
-      if (array[i] == tofind)
-      {
+bool HasValue(T array[], unsigned int size, T tofind) {
+  if (size > 0) {
+    for (unsigned int i = 0; i < size; i++) {
+      if (array[i] == tofind) {
         return true;
       }
     }
@@ -106,15 +92,11 @@ bool HasValue(T array[], unsigned int size, T tofind)
  *               you can return T()
  */
 template <typename T>
-T ValueAt(T array[], unsigned int size, unsigned int index, bool &error)
-{
+T ValueAt(T array[], unsigned int size, unsigned int index, bool &error) {
   error = false;
-  if (index < size)
-  {
+  if (index < size) {
     return array[index];
-  }
-  else
-  {
+  } else {
     error = true;
     return T();
   }
@@ -128,11 +110,9 @@ T ValueAt(T array[], unsigned int size, unsigned int index, bool &error)
  * @return T - The sum of the values in the array
  */
 template <typename T>
-T Sum(T array[], unsigned int size)
-{
+T Sum(T array[], unsigned int size) {
   T output = T();
-  for (unsigned int i = 0; i < size; i++)
-  {
+  for (unsigned int i = 0; i < size; i++) {
     output += array[i];
   }
   return output;
@@ -149,17 +129,13 @@ T Sum(T array[], unsigned int size)
  */
 template <typename T>
 bool SwapValues(T array[], unsigned int size, unsigned int index1,
-                unsigned int index2)
-{
-  if (index1 <= size && index2 <= size)
-  {
+                unsigned int index2) {
+  if (index1 <= size && index2 <= size) {
     T temp = array[index1];
     array[index1] = array[index2];
     array[index2] = temp;
     return true;
-  }
-  else
-  {
+  } else {
     return false;
   }
 }
